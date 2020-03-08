0fe1a2168c929764d7dcaec5b72447ec70e9bb6b
dervish <josueortigozaramos@gmail.com>
2020-03-07 21:45:22 -0800

    Mecánica general terminada

--- Assets/Scripts/FeedbackControl.cs
+++ Assets/Scripts/FeedbackControl.cs
@@ -4,15 +4,79 @@ using UnityEngine;
 
 public class FeedbackControl : MonoBehaviour
 {
+
+    public int idButton;
+    private float lightinstensity;
+    public Light Llum;
+    public bool Desactivate;
+    public bool desactivating;
+    public AudioClip buttonclick;
+    public SimonManager controlador;
+
+    /*Color baseColor;
+    Renderer renderer_;
+    Material mat;
+    float emission;
+    */
+
     // Start is called before the first frame update
     void Start()
     {
+        lightinstensity = Llum.intensity;
         
+
+        
+    }
+
+    public void ActivateButton()
+    {
+        desactivating = false;
+        Desactivate = false;
+        Llum.intensity = lightinstensity;
+        Llum.gameObject.SetActive(true);
+
+        //Llamar al manager para decirle que hemos hecho click a éste botón
+
+        if (controlador.turnoPlayer)
+        {
+            controlador.ClickUsuario(idButton);
+
+        }
+        
+        AudioSource.PlayClipAtPoint(buttonclick, Vector3.zero, 1.0f);
+
+        Invoke("desactivarCubo", 0.1f);
+
+    }
+
+    public void desactivarCubo()
+    {
+        desactivating = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if(desactivating && !Desactivate)
+        {
+            Llum.intensity = Mathf.Lerp(Llum.intensity, 0, 0.065f); 
+
+        }
+
+        if (Llum.intensity <= 0.02)
+        {
+            Llum.intensity = 0;
+            Desactivate = true;
+        }
         
     }
+
+    public void OnMouseDown()
+    {
+        print(name);
+        ActivateButton();
+    }
+
 }
--- Assets/Scripts/SimonManager.cs
+++ Assets/Scripts/SimonManager.cs
@@ -1,18 +1,149 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class SimonManager : MonoBehaviour
 {
+
+    public FeedbackControl[] buttons;
+    public List<int> ListaAleatoria = new List<int>();
+
+    public bool ListaLlena;
+    public bool turnoPc;
+    public bool turnoPlayer;
+    
+
+    public int Contador;
+    public int ContadorUsuario;
+    public int NivelActual;
+
+    [Range(0.1f, 2f)]
+    public float Velocidad;
+
+    //public Slider sliderVel;
+    //public Text SliderText;
+    //public Text gameOverText;
+    //public Text Nivel;
+
+
     // Start is called before the first frame update
-    void Start()
+
+
+    private void Start()
     {
-        
+
+        LlenarListaAleatoria();
+        turnoPc = true;
+        Invoke("TurnoPC", 0.5f);
+
+    }
+    void LlenarListaAleatoria()
+    {
+        for (int i = 0; i <= 1000; i++)
+        {
+            ListaAleatoria.Add(Random.Range(0, 4));
+
+        }
+        ListaLlena = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void cambiarEstado()
     {
-        
+        if (turnoPc)
+        {
+            turnoPc = false;
+            turnoPlayer = true;
+
+        }
+        else
+        {
+            turnoPc = true;
+            turnoPlayer = false;
+            Contador = 0;
+            ContadorUsuario = 0;
+            Invoke("TurnoPC", 1.2f);
+        }
+
     }
+
+    void TurnoPC()
+    {
+        if (ListaLlena && turnoPc)
+        {
+            buttons[ListaAleatoria[Contador]].ActivateButton();
+
+
+            if (Contador >= NivelActual)
+            {
+                NivelActual++;
+                cambiarEstado();
+
+            }
+            else
+            {
+                Contador++;
+            }
+            Invoke("TurnoPC", Velocidad);
+
+        }
+    }
+
+
+    public void ClickUsuario(int idButton)
+    {
+        if (idButton != ListaAleatoria[ContadorUsuario])
+        {
+            print("Game Over");
+            GameOver();
+            return;
+
+        }
+
+        if (ContadorUsuario == Contador)
+        {
+
+            cambiarEstado();
+        }else
+        {
+
+            ContadorUsuario++;
+        }
+
+    }
+
+    public void GameOver()
+    {
+
+        turnoPc = false;
+        turnoPlayer = false;
+
+    }
+
+    public void Reiniciar()
+    {
+        Contador = 0;
+        ContadorUsuario = 0;
+        NivelActual = 0;
+        ListaAleatoria.Clear();
+        LlenarListaAleatoria();
+        turnoPc = true;
+        Invoke("TurnoPC", 1f);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+
+            Reiniciar();
+        }
+    }
+
+
 }
+
+
+
+
